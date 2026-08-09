8e853452c5c17464257e78a2b3f07f5ca159cf87
vishal-inkollu <sai.vishal@spectraforce.com>
2026-08-09 16:15:04 +0530

    frontend url change

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api/v1"
+    baseURL: "https://salary-management-backend-ujai.onrender.com/api/v1"
 });
 
 export default api;
